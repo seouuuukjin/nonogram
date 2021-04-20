fc06eab2781dd1f890b8d022827353f077e58aab
seouuuukjin <uukjin4@gmail.com>
2021-04-20 21:57:16 +0900

    OkHttp 설치 완료 이제 manifest 설정하고 시작 하면 됨

---
+++ app/src/main/java/edu/skku/map/nonogram/NaverAPI.java
@@ -0,0 +1,14 @@
+package edu.skku.map.nonogram;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+
+public class NaverAPI {
+    String clientID = "KRIEpEMi28TqAmxIyo5Y";
+    String ClientSecret = "5RBthImUlc";
+
+    public String NaverSearch(String keyword){
+        OkHttpClient client = new OkHttpClient();
+        return null;
+    }
+}
